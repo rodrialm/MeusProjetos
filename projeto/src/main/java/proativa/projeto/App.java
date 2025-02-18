58db08304fa80d3f519a2cba7f345b3a69ab7767
rodrialm <rodrigoalmeidab@outlook.com>
2025-02-18 15:28:16 -0300

    primeiro passo

---
+++ projeto/src/main/java/proativa/projeto/App.java
@@ -0,0 +1,13 @@
+package proativa.projeto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
